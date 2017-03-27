e55d76a3fc7b9fc0b4b240ce032fa61adf985453
Nam Nguyen <NamNguyen@Nams-MacBook-Pro.local>
2017-03-27 12:01:11 -0400

    First Commit

---
+++ app/src/main/java/cis/gvsu/edu/geocalculator/GeoCalculatorApp.java
@@ -0,0 +1,17 @@
+package cis.gvsu.edu.geocalculator;
+
+import android.app.Application;
+
+import net.danlew.android.joda.JodaTimeAndroid;
+
+/**
+ * Created by NamNguyen on 3/24/17.
+ */
+
+public class GeoCalculatorApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        JodaTimeAndroid.init(this);
+    }
+}
